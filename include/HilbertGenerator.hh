00c334a70621dc650a9fcffe476a71318d08d416
Wojciech Jabłoński <wj359634@students.mimuw.edu.pl>
2016-08-31 00:13:41 +0200

    glm compatibility fix

--- include/HilbertGenerator.hh
+++ include/HilbertGenerator.hh
@@ -3,6 +3,12 @@
 #include <glm/glm.hpp>
 #include <cstdint>
 
+//fix for derpy old GLM versions
+#if GLM_VERSION < 97
+namespace glm {
+ template<typename T, precision P> using tmat3x3 = detail::tmat3x3<T, P>;
+}
+#endif
 typedef glm::tmat3x3<int, glm::precision::highp> imat3;
 typedef glm::ivec3 ivec3;
 
